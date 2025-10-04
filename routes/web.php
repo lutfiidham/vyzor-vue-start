cdd48a5b8f9ee54c99fc8208831b3da3a8f46f24
Lutfi Idham <lutfiidham0.0@gmail.com>
2025-10-05 01:35:03 +0700

    install pint

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('', function () {
     return Inertia::render('Login'); // your React component
