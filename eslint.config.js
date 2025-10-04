05a54799c8c1aac865faf7e77a0cacc5ef160185
Lutfi Idham <lutfiidham0.0@gmail.com>
2025-10-04 17:57:28 +0700

    update eslint file vue

--- eslint.config.js
+++ eslint.config.js
@@ -3,31 +3,43 @@ import pluginVue from 'eslint-plugin-vue'
 import pluginPrettier from 'eslint-plugin-prettier'
 import globals from 'globals'
 
+// 🧠 import parser Vue
+import vueParser from 'vue-eslint-parser'
+
 export default [
   js.configs.recommended, // aturan dasar JS
 
   {
     files: ['resources/js/**/*.{js,vue}'],
+
+    // 🧩 tambahkan parser agar bisa baca <template> & <script setup>
     languageOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
+      parser: vueParser,
+      parserOptions: {
+        parser: '@babel/eslint-parser', // biar dukung sintaks modern JS
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: { jsx: false },
+      },
       globals: {
         ...globals.browser,
         ...globals.node,
       },
     },
+
     plugins: {
       vue: pluginVue,
       prettier: pluginPrettier,
     },
+
     rules: {
-      // tambahkan aturan vue manual biar gak error
+      // Aturan Vue 3 dasar
       ...pluginVue.configs['flat/recommended'].rules,
 
       // Nonaktifkan aturan yang terlalu ketat
       'vue/multi-word-component-names': 'off',
 
-      // Integrasi prettier
+      // Integrasi Prettier
       'prettier/prettier': [
         'error',
         {
