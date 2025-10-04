fe6bc9dc353e61206717533b2a13a3e14e97fed8
Lutfi Idham <lutfiidham0.0@gmail.com>
2025-10-04 18:07:41 +0700

    update lint ignore babel

--- eslint.config.js
+++ eslint.config.js
@@ -2,21 +2,18 @@ import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import pluginPrettier from 'eslint-plugin-prettier'
 import globals from 'globals'
-
-// 🧠 import parser Vue
 import vueParser from 'vue-eslint-parser'
 
 export default [
-  js.configs.recommended, // aturan dasar JS
+  js.configs.recommended,
 
   {
     files: ['resources/js/**/*.{js,vue}'],
-
-    // 🧩 tambahkan parser agar bisa baca <template> & <script setup>
     languageOptions: {
       parser: vueParser,
       parserOptions: {
-        parser: '@babel/eslint-parser', // biar dukung sintaks modern JS
+        parser: '@babel/eslint-parser',
+        requireConfigFile: false, // ⬅️ tambahkan ini biar gak butuh .babelrc
         ecmaVersion: 2020,
         sourceType: 'module',
         ecmaFeatures: { jsx: false },
@@ -26,20 +23,13 @@ export default [
         ...globals.node,
       },
     },
-
     plugins: {
       vue: pluginVue,
       prettier: pluginPrettier,
     },
-
     rules: {
-      // Aturan Vue 3 dasar
       ...pluginVue.configs['flat/recommended'].rules,
-
-      // Nonaktifkan aturan yang terlalu ketat
       'vue/multi-word-component-names': 'off',
-
-      // Integrasi Prettier
       'prettier/prettier': [
         'error',
         {
